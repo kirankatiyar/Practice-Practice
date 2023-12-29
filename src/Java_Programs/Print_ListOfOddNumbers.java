113711e1cc7da46f2ce85c702e2de666ecb8920d
“kirankatiyar” <kirankatiyar16@gmail.com>
2023-12-29 19:58:35 +0530

    list of odd number

--- src/Java_Programs/Print_ListOfOddNumbers.java
+++ src/Java_Programs/Print_ListOfOddNumbers.java
@@ -1,4 +1,15 @@
 package Java_Programs;
-
+import java.util.Scanner;
 public class Print_ListOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number ,From 1 to witch number ,you want print odd number");
+        int no = sc.nextInt();
+        System.out.println("List of odd numbers from 1 to"+" "+no);
+        for(int i=1;i<=no;i++) {
+            if(i%2!=0){
+                System.out.print(i+",");
+            }
+        }
+    }
 }
